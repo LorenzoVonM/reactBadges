594cec270affd62acdf06461eb56356aceef7f81
Leonardo Flores <nadeshkokeane@hotmail.com>
2019-11-10 14:01:22 -0600

    Added navbar page

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
-import Badge from './components/Badge';
+import BadgeNew from './pages/BadgeNew';
 
 //const element = <h1>Hello, Platzi Badgesdddd!</h1>;
 
@@ -18,12 +18,6 @@ const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 ReactDOM.render(
-  <Badge 
-    firstName='Leo' 
-    lastName='Hernández'
-    jobTitle='Fullstack Dev'
-    twitter='leo_Supernova'
-    avatarUrl='https://s.gravatar.com/avatar/6d0594db0a0ade9b4302932bd113a271?s=80'
-  />, 
+  <BadgeNew />, 
   container
 );
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import './styles/BadgeNew.css';
+import Navbar from '../components/Navbar';
+import header from '../images/badge-header.svg';
+import Badge from '../components/Badge';
+
+class BadgeNew extends React.Component {
+  render(){
+    return(
+      <div>
+        <Navbar />
+        <div className='BadgeNew__hero'>
+          <img className='img-fluid' src={header} alt='Logo' />
+        </div>
+        <div className='container'>
+          <div className='row'>
+            <div className='col'>
+              <Badge firstName='Leo' 
+                lastName='Hernández'
+                jobTitle='Fullstack Dev'
+                twitter='leo_Supernova'
+                avatarUrl='https://s.gravatar.com/avatar/6d0594db0a0ade9b4302932bd113a271?s=80'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BadgeNew;
