ca7b6f8a6fc5bf9aa9ba10ca7dd31784a3aa1540
Leonardo Flores <nadeshkokeane@hotmail.com>
2019-11-18 22:34:21 -0600

    added loading and server errors pages for better ux

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
+import PageLoading from '../components/PageLoading';
+import PageError from '../components/PageError';
 
 import api from '../api';
  
@@ -105,11 +107,11 @@ class Badges extends React.Component {
   render() {
 
     if(this.state.loading === true){
-      return 'loading...';
+      return <PageLoading />;
     }
 
     if(this.state.error){
-      return `Error: ${this.state.error.message}`;
+      return <PageError error={this.state.error}/>;
     }
 
     return(
